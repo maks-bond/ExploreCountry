36fd83f503dabd2364d5c34ddb20e5832c7b48d2
Maksym Bondarenko <mabondaren@gmail.com>
2016-01-30 11:35:18 -0800

    Added countries list

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-$(function(){console.log("AAAA")})
+/* global $ */
+
+var airports = airportData.airports;
+var countries = {};
+function populateCountries(){
+    for (var i = 0; i<airports.length; ++i) {
+        var airport = airports[i];
+        var country = airport.country;
+        countries[country] = true;
+    }
+}
+
+$(function(){
+   populateCountries();
+});
+
+
+
+
+
+    
