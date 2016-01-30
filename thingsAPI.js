2154fb50bbeedf5d192ddb973cfe683bae94c3e6
irusiabondarenko <irusiabondarenko@gmail.com>
2016-01-30 12:09:54 -0800

    retrieved data from api, passed to object

--- thingsAPI.js
+++ thingsAPI.js
@@ -1,14 +1,16 @@
+
 function showThings () {
   $.ajax({
     url: "http://terminal2.expedia.com:80/x/activities/search?location=London&startDate=2016-08-08&endDate=2016-08-18&apikey=lZg5sVj3LGQC7PZFGX6tkAw2mwAzyINJ",
     type: "get",
     success: function(res) { 
-
+      var thingsObject = {};
       var things = [];
-      var obj = res;
-      var results = obj.activities;
+      var results = res.activities;
       for (var i = 0; i<results.length; i++) {
-        things.push(results[i].imageUrl);
+        thingsObject.title = results[i].title;
+        thingsObject.image = results[i].imageUrl;
+        things.push(thingsObject);
       }
       console.log(things);
     }
